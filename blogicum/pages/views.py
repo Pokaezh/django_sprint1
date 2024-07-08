30111f812d3dba7b561970e05325170e2fddfac6
Nikolai Pokazanev <pokaezh@yandex.ru>
2024-07-08 14:50:02 +0300

    pages_views_fix

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -3,11 +3,9 @@ from django.shortcuts import render
 
 def about(request):
     """Функция для отображения страницы о проекте"""
-
     return render(request, "pages/about.html")
 
 
 def rules(request):
     """Функция для отображения страницы с правилами проекта"""
-
     return render(request, "pages/rules.html")
